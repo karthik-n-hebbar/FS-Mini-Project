15cd8f9856d94269bfde373e9d08010fc3cd2f3f
karthik-n-hebber <karthiknh125@gmail.com>
2023-05-31 20:59:51 +0530

    home.html is executed

--- fs_mini_project/fs_mini_project/settings.py
+++ fs_mini_project/fs_mini_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'fs_mini_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,5 +121,10 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+# Added manually
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static") 
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
