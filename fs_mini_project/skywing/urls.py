9f53ec1ad050ab06e38bce2f1fc3b9a3ed4bfc7b
karthik-n-hebber <karthiknh125@gmail.com>
2023-06-12 00:48:52 +0530

    Booking.html

--- fs_mini_project/skywing/urls.py
+++ fs_mini_project/skywing/urls.py
@@ -6,5 +6,6 @@ from skywing import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('booking', views.booking, name='booking'),
 ]
--- fs_mini_project/skywing/views.py
+++ fs_mini_project/skywing/views.py
@@ -11,3 +11,6 @@ def contact(request):
 
 def about(request):
     return render(request, "about.html")
+
+def booking(request):
+    return render(request, "booking.html")
