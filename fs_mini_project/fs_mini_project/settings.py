24c69f306a66f1e450b6b1e3a4169cf05e2b4f89
karthik-n-hebber <karthiknh125@gmail.com>
2023-06-11 19:05:10 +0530

    Initial Web Development

--- fs_mini_project/fs_mini_project/settings.py
+++ fs_mini_project/fs_mini_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'skywing'
 ]
 
 MIDDLEWARE = [
@@ -117,14 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 # Added manually
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static") 
+    os.path.join(BASE_DIR, 'static') 
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
