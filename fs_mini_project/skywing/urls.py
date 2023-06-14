277b5c6f06b696a47b0837d78f1bf3a355385dbc
karthik-n-hebber <karthiknh125@gmail.com>
2023-06-14 23:34:21 +0530

    Home Page 3 Tabs

--- fs_mini_project/skywing/urls.py
+++ fs_mini_project/skywing/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+#from skywing.views import view_file
 
 
 from skywing import views
@@ -7,5 +8,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
     path('about', views.about, name='about'),
-    path('booking', views.booking, name='booking'),
+    path('booking', views.booking, name='booking')
+    #path('booking', views.view_file, name='view_file')
 ]
--- fs_mini_project/skywing/views.py
+++ fs_mini_project/skywing/views.py
@@ -12,5 +12,19 @@ def contact(request):
 def about(request):
     return render(request, "about.html")
 
+
+
 def booking(request):
     return render(request, "booking.html")
+    file_path = 'fs_mini_project\skywing\booking.txt'  # Replace with the actual file path in your project's base directory
+
+    with open(file_path, 'r') as file:
+        file_contents = file.read()
+
+    context = {'file_contents': file_contents}
+    return render(request, 'booking.html', context)
+
+from django.shortcuts import render
+
+#def view_file(request):
+    
